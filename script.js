e52ac4d9dccf4166070a5c8b6487f79d33960922
bakomans <bakomans@gmail.com>
2023-11-21 12:16:20 +0000

    finish styling the page

--- script.js
+++ script.js
@@ -1,118 +1,92 @@
-// creating Dom and Variables
-document.addEventListener('DOMContentLoaded', function () {
-    const apiKey = 'a4a04040b26ae7120449f277bffe523d';
-    const searchForm = document.getElementById('search-form');
-    const searchInput = document.getElementById('search-input');
-    const todaySection = document.getElementById('today');
-    const forecastSection = document.getElementById('forecast');
-    const historyList = document.getElementById('history');
+$(document).ready(function () {
+    // My Api key
+    const apiKey = "a4a04040b26ae7120449f277bffe523d";
   
-    searchForm.addEventListener('submit', function (e) {
-      e.preventDefault();
-      const city = searchInput.value.trim();
+    // Event listener for the form 
+    $("#search-form").submit(function (event) {
+      event.preventDefault();
+      const cityName = $("#search-input").val().trim();
   
-      if (city) {
-        // Fetch current weather
-        fetchWeatherData(city);
+      if (cityName !== "") {
+        $("#search-input").val("");
+        getWeather(cityName);
       }
     });
   
-    historyList.addEventListener('click', function (e) {
-      if (e.target.matches('.history-item')) {
-        const city = e.target.textContent;
-        // Fetch weather data for the selected city
-        fetchWeatherData(city);
-      }
-    });
+    // Function to fetch weather data
+    function getWeather(cityName) {
+      const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
   
-    function fetchWeatherData(city) {
-      // Call OpenWeatherMap API to get weather data
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-        .then(response => response.json())
-        .then(data => {
-          // Display current weather
-          displayCurrentWeather(data);
-  
-          // Add city to search history
-          addToSearchHistory(city);
-  
-          // Call OpenWeatherMap API to get 5-day forecast
-          return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`);
-        })
-        .then(response => response.json())
-        .then(data => {
-          // Display 5-day forecast
-          displayForecast(data);
-        })
-        .catch(error => console.error('Error fetching weather data:', error));
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        displayCurrentWeather(response);
+        displayForecast(response);
+        addToHistory(cityName);
+      });
     }
   
+    // Function to display current weather
     function displayCurrentWeather(data) {
-      // Implement code to display current weather
-      todaySection.innerHTML = `
-        <h2>${data.name} - ${dayjs().format('MMMM D, YYYY')}</h2>
-        <p>Temperature: ${data.main.temp}°C</p>
-        <p>Humidity: ${data.main.humidity}%</p>
-        <p>Wind Speed: ${data.wind.speed} m/s</p>
+      const currentWeather = data.list[0];
+      const city = data.city.name;
+      const date = dayjs().format("MM/DD/YYYY");
+      const iconCode = currentWeather.weather[0].icon;
+      const iconURL = `https://openweathermap.org/img/wn/${iconCode}.png`;
+      const temperature = currentWeather.main.temp;
+      const humidity = currentWeather.main.humidity;
+      const windSpeed = currentWeather.wind.speed;
+  
+      const currentWeatherHTML = `
+        <h2>${city} (${date}) <img src="${iconURL}" alt="Weather Icon"></h2>
+        <p>Temperature: ${temperature} &#8457;</p>
+        <p>Humidity: ${humidity}%</p>
+        <p>Wind Speed: ${windSpeed} MPH</p>
       `;
+  
+      $("#today").html(currentWeatherHTML);
     }
   
+    // Function to display 5-day forecast
     function displayForecast(data) {
-      // Implement code to display 5-day forecast
-      forecastSection.innerHTML = '<h2>5-Day Forecast</h2>';
-      for (let i = 0; i < data.list.length; i += 8) {
-        const forecast = data.list[i];
-        forecastSection.innerHTML += `
-          <div class="col-md-2">
-            <p>Date: ${dayjs(forecast.dt_txt).format('MMMM D, YYYY')}</p>
-            <p>Temperature: ${forecast.main.temp}°C</p>
-            <p>Humidity: ${forecast.main.humidity}%</p>
-          </div>
-        `;
+        const forecast = data.list.slice(1, 6);
+      
+        let forecastHTML = "";
+        forecast.forEach((item) => {
+          const date = dayjs(item.dt_txt).format("MM/DD/YYYY");
+          const iconCode = item.weather[0].icon;
+          const iconURL = `https://openweathermap.org/img/wn/${iconCode}.png`;
+          const temperature = item.main.temp;
+          const humidity = item.main.humidity;
+      
+          forecastHTML += `
+            <div class="col-md">
+              <div class="card bg-primary text-white">
+                <div class="card-body">
+                  <h5>${date}</h5>
+                  <img src="${iconURL}" alt="Weather Icon">
+                  <p>Temp: ${temperature} &#8457;</p>
+                  <p>Humidity: ${humidity}%</p>
+                </div>
+              </div>
+            </div>
+          `;
+        });
+      
+        $("#forecast").html(forecastHTML);
       }
-    }
   
-    function addToSearchHistory(city) {
-      // Add city to search history
-      const historyItem = document.createElement('div');
-      historyItem.textContent = city;
-      historyItem.classList.add('history-item', 'list-group-item', 'list-group-item-action');
-      historyList.prepend(historyItem);
+    // Function to add the city to the search 
+    function addToHistory(cityName) {
+      const historyItem = `<button type="button" class="list-group-item">${cityName}</button>`;
+      $("#history").prepend(historyItem);
+  
+      // Event listener for history item 
+      $(".list-group-item").click(function () {
+        const selectedCity = $(this).text();
+        getWeather(selectedCity);
+      });
     }
   });
-  function displayCurrentWeather(data) {
-    todaySection.innerHTML = `
-      <h2>${data.name} - ${dayjs().format('MMMM D, YYYY')}</h2>
-      <p>Temperature: ${data.main.temp}°C</p>
-      <p>Humidity: ${data.main.humidity}%</p>
-      <p>Wind Speed: ${data.wind.speed} m/s</p>
-      <p>Weather: ${data.weather[0].description} <i class="fas fa-${getWeatherIcon(data.weather[0].icon)}"></i></p>
-    `;
-  }
-  
-  function getWeatherIcon(iconCode) {
-    // Map OpenWeatherMap icon codes to FontAwesome icons
-    const iconMap = {
-      '01d': 'sun',
-      '01n': 'moon',
-      '02d': 'cloud-sun',
-      '02n': 'cloud-moon',
-      '03d': 'cloud',
-      '03n': 'cloud',
-      '04d': 'cloud',
-      '04n': 'cloud',
-      '09d': 'cloud-showers-heavy',
-      '09n': 'cloud-showers-heavy',
-      '10d': 'cloud-sun-rain',
-      '10n': 'cloud-moon-rain',
-      '11d': 'bolt',
-      '11n': 'bolt',
-      '13d': 'snowflake',
-      '13n': 'snowflake',
-      '50d': 'smog',
-      '50n': 'smog',
-    };
-  
-    return iconMap[iconCode] || 'question-circle';
-  }
   
